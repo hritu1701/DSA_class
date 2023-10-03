4637ea6caef5f70c96c581c5210de948af1ff0d9
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-10-03 08:19:21 +0530

    updated priority queue

---
+++ priority_queue.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout<<"Enter the size: ";
+    cin>>size;
+
+    int arr[size];
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+	
+	priority_queue<int> pq;
+	cout << "Array: ";
+	for (auto i : arr) {
+		cout << i <<" ";
+	}
+	cout << endl;
+	
+	for (int i = 0; i < size; i++) {
+		pq.push(arr[i]);
+	}
+
+
+	cout << "Priority Queue: ";
+	while (!pq.empty()) {
+		cout << pq.top() << ' ';
+		pq.pop();
+	}
+
+	return 0;
+}
