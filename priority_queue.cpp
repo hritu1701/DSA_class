a2a5604aba7dacf5bfd21978eddec780b10d0183
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-10-03 08:27:33 +0530

    Update priority_queue.cpp

--- priority_queue.cpp
+++ priority_queue.cpp
@@ -10,14 +10,14 @@ int main()
     cin>>size;
 
     int arr[size];
-    cout<<"Enter the elements of array: ";
+    cout<<"Enter the elements of Queue: ";
     for(int i=0;i<size;i++)
     {
         cin>>arr[i];
     }
 	
 	priority_queue<int> pq;
-	cout << "Array: ";
+	cout << "Queue is: ";
 	for (auto i : arr) {
 		cout << i <<" ";
 	}
